23b209ec5e6ddb8e5a48edab3d7742413f884444
Emil Stanimirov <145929264+EmilStanimirov90@users.noreply.github.com>
2024-06-11 22:17:37 +0300

    ex

---
+++ 07Database Programmability/Exercise/code/03.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION fn_is_word_comprised(
+	set_of_letters VARCHAR(50),
+	word VARCHAR(50)
+) RETURNS BOOLEAN
+AS
+$$
+BEGIN
+	RETURN TRIM(LOWER(word), LOWER(set_of_letters)) = '';
+END;
+$$
+LANGUAGE plpgsql;
+
+SELECT * FROM fn_is_word_comprised('ois tmiah%f', 'halves')
+SELECT * FROM fn_is_word_comprised('ois tmiah%f', 'Sofia')
+SELECT * FROM fn_is_word_comprised('bobr', 'Rob')
+SELECT * FROM fn_is_word_comprised('papopep', 'toe')
+SELECT * FROM fn_is_word_comprised('R@o!B$B', 'Bob')

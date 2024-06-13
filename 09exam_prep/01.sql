610f6ac1083355ac009a5efd5ed9119a28f65def
Emil Stanimirov <145929264+EmilStanimirov90@users.noreply.github.com>
2024-06-14 00:09:03 +0300

    ex

---
+++ 09exam_prep/01.sql
@@ -0,0 +1,65 @@
+CREATE TABLE addresses(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE categories(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(10) NOT NULL
+);
+CREATE TABLE clients(
+	id SERIAL PRIMARY KEY,
+	full_name VARCHAR(50) NOT NULL,
+	phone_number VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE drivers(
+	id SERIAL PRIMARY KEY,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	age INT NOT NULL,
+	rating NUMERIC(2) DEFAULT 5.5,
+
+	CONSTRAINT ck_age
+		CHECK(age > 0)
+	
+);
+
+CREATE TABLE cars(
+	id SERIAL PRIMARY KEY,
+	make VARCHAR(20) NOT NULL,
+	model VARCHAR(20),
+	year INT NOT NULL DEFAULT 0,
+	mileage INT DEFAULT 0,
+	condition CHAR(1) NOT NULL,
+	category_id INT NOT NULL,
+	CONSTRAINT fk_category
+		FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT ck_year
+		CHECK (year > 0),
+	CONSTRAINT ck_mileage
+		CHECK (mileage > 0)
+	);
+
+CREATE TABLE courses(
+	id SERIAL PRIMARY KEY,
+	from_address_id INT NOT NULL,
+	FOREIGN KEY (from_address_id) REFERENCES addresses(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	start TIMESTAMP NOT NULL,
+	bill NUMERIC(10,2)  DEFAULT 10,
+	CONSTRAINT ck_bill
+		CHECK(bill > 0),
+	car_id INT NOT NULL,
+	FOREIGN KEY (car_id) REFERENCES cars(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	client_id INT NOT NULL,
+	FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE cars_drivers(
+	car_id INT NOT NULL,
+	driver_id INT NOT NULL,
+	CONSTRAINT fk_cars
+		FOREIGN KEY (car_id) REFERENCES cars(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT fk_driver
+	FOREIGN KEY (driver_id) REFERENCES drivers(id ) ON DELETE CASCADE ON UPDATE CASCADE
+);
